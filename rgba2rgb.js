ccd84aa32ae546802e0c274b2f61cfa487558a94
Rahul Vats <vats.rahul@gmail.com>
2019-07-22 15:30:04 -0500

    Update rgba2rgb.js

--- rgba2rgb.js
+++ rgba2rgb.js
@@ -1,19 +1,15 @@
+function Color(r, g, b, a) {
+	this.r = r;
+  this.b = b;
+  this.g = g;
+  this.a = typeof a == "number" ? a : 1;
+}
 
-
-  function Color(r, g, b, a) {
-      this.r = r;
-      this.b = b;
-      this.g = g;
-      this.a = typeof a == "number" ? a : 1;
-  }
-
-  Color.prototype.rgba2rgb = function (color)
-  {
-      var alpha = color.a;
-
-      return new Color(
-          (1 - alpha) * this.r + alpha * color.r,
-          (1 - alpha) * this.g + alpha * color.g,
-          (1 - alpha) * this.b + alpha * color.b
-      );
-  };
+function rgba2rgb(RGB_background, RGBA_color) {
+  var alpha = RGBA_color.a;
+  return new Color(
+    (1 - alpha) * RGB_background.r + alpha * RGBA_color.r,
+    (1 - alpha) * RGB_background.g + alpha * RGBA_color.g,
+    (1 - alpha) * RGB_background.b + alpha * RGBA_color.b
+  );
+}
